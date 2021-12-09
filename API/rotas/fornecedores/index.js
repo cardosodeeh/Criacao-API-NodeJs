3c00e548c9d93b73c9b48381b7612ef1ec7528de
Andreza Cardoso <77634842+cardosodeeh@users.noreply.github.com>
2021-12-09 14:46:12 -0300

    Adicionado testes práticos do curso de API

---
+++ API/rotas/fornecedores/index.js
@@ -0,0 +1,21 @@
+const roteador = require('express').Router()
+const TabelaFornecedor = require('./TabelaFornecedor')
+const Fornecedor = require('./fornecedor')
+
+roteador.get('/', async (requisicao, resposta) => {
+    const resultados = await TabelaFornecedor.listar()
+    resposta.send(
+        JSON.stringify(resultados)
+    )
+})
+
+roteador.post('/', async (requisicao, resposta) => {
+    const dadosRecebidos = requisicao.body
+    const fornecedor = new Fornecedor(dadosRecebidos)
+    await fornecedor.criar()
+    resposta.send(
+        JSON.stringify(fornecedor)
+    )
+})
+
+module.exports = roteador
