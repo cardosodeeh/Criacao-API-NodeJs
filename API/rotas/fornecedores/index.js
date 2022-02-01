577ea7fec4e3129f3b33c23100870fce09621837
Andreza Cardoso <77634842+cardosodeeh@users.noreply.github.com>
2022-02-01 18:56:45 -0300

    método delete e put

--- API/rotas/fornecedores/index.js
+++ API/rotas/fornecedores/index.js
@@ -4,18 +4,29 @@ const Fornecedor = require('./fornecedor')
 
 roteador.get('/', async (requisicao, resposta) => {
     const resultados = await TabelaFornecedor.listar()
+    resposta.status(200)
     resposta.send(
         JSON.stringify(resultados)
     )
 })
 
 roteador.post('/', async (requisicao, resposta) => {
-    const dadosRecebidos = requisicao.body
-    const fornecedor = new Fornecedor(dadosRecebidos)
-    await fornecedor.criar()
-    resposta.send(
-        JSON.stringify(fornecedor)
-    )
+    try {
+        const dadosRecebidos = requisicao.body
+        const fornecedor = new Fornecedor(dadosRecebidos)
+        await fornecedor.criar()
+        resposta.status(201)
+        resposta.send(
+            JSON.stringify(fornecedor)
+        )
+    } catch (erro) {
+        resposta.status(400)
+        resposta.send(
+            JSON.stringify({
+                mensagem: erro.message
+            })
+        )
+    }
 })
 
 roteador.get('/:idFornecedor', async (requisicao, resposta) => {
@@ -23,6 +34,7 @@ roteador.get('/:idFornecedor', async (requisicao, resposta) => {
         const id = requisicao.params.idFornecedor
         const fornecedor = new Fornecedor({id: id})
         await fornecedor.carregar()
+        resposta.status(200)
         resposta.send(
             JSON.stringify(fornecedor)
         )
@@ -42,6 +54,7 @@ roteador.put('/:idFornecedor', async (requisicao, resposta) => {
         const dados = Object.assign({}, dadosRecebidos, {id: id})
         const fornecedor = new Fornecedor(dados)
         await fornecedor.atualizar()
+        resposta.status(204)
         resposta.end()
     } catch (erro) {
         resposta.send(
@@ -57,6 +70,8 @@ roteador.delete('/:idFornecedor', async (requisicao, resposta) => {
         const id = requisicao.params.idFornecedor
         const fornecedor = new Fornecedor({id: id})
         await fornecedor.carregar()
+        await fornecedor.remover()
+        resposta.end()
     } catch (erro) {
         resposta.send(
             JSON.stringify({
