6396a4475fed8a6f64f2b8e4c962241ada0dd028
Andreza Cardoso <77634842+cardosodeeh@users.noreply.github.com>
2022-02-14 19:40:34 -0300

    Finalização do curso

--- API/index.js
+++ API/index.js
@@ -5,8 +5,28 @@ const config = require('config')
 const NaoEncontrado = require('./erros/NaoEncontrado')
 const CampoInvalido = require('./erros/CampoInvalido')
 const DadosNaoFornecidos = require('./erros/DadosNaoFornecidos')
+const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
+const formatosAceitos = require('./Serializador').formatosAceitos
+const SerializadorErro = require('./Serializador').SerializadorErro
 
 app.use(bodyParser.json())
+
+app.use((requisicao, resposta, proximo) => {
+    let formatoRequisitado = requisicao.header('Accept')
+
+    if (formatoRequisitado === '*/*') {
+        formatoRequisitado = 'application/json'
+    }
+
+    if (formatosAceitos.indexOf(formatoRequisitado) === -1) {
+        resposta.status(406)
+        resposta.end()
+        return
+    }
+
+    resposta.setHeader('Content-Type', formatoRequisitado)
+    proximo()
+})
 
 const roteador = require('./rotas/fornecedores')
 app.use('/api/fornecedores', roteador)
@@ -20,9 +40,17 @@ app.use((erro, requisicao, resposta, proximo) => {
     if (erro instanceof CampoInvalido || erro instanceof DadosNaoFornecidos) {
         status = 400
     }
+
+    if (erro instanceof ValorNaoSuportado) {
+        status = 406
+    }
+
+    const serializador = new SerializadorErro(
+        resposta.getHeader('Content-Type')
+    )
     resposta.status(status)
     resposta.send(
-        JSON.stringify({
+        serializador.serializar({
             mensagem: erro.message,
             id: erro.idErro
         })
